831324b3a87209fbbe172f7926e12254ee18b593
Tobias Troesch <Tobias.Troesch@medavis.de>
2025-06-18 20:42:16 +0200

    Using maven with main class

--- src/main/com/example/myapp/MyApp.java
+++ src/main/com/example/myapp/MyApp.java
@@ -1,10 +1,11 @@
 package com.example.myapp;
 
+import java.time.LocalDateTime;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.*;
-
-import java.time.LocalDateTime;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
